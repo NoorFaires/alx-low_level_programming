8b1235d0d8fea6ad345cc61166bf502b07149d6d
Noor <nourelhoudalfaires@gmail.com>
2023-03-17 17:45:52 +0000

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**
   * main - Entry point
+  *
   * Description : A C program that prints with puts function
-  * Return : Always returns 0 (Success)
-  */
+  *
+  * Return : Returns 0 (Success)
+ */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
