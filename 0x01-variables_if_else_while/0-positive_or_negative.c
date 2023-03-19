f98fd78397cfb135d39d1aca0576e82059a4fa5c
Noor <nourelhoudalfaires@gmail.com>
2023-03-19 09:21:24 -0700

    ifelsecommit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program that prints if the random number.
+ *
+ * Return: return 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0);
+	n = rand() - RAND_MAX / 2;
+	if  (n > 0)
+	{
+	printf("%d is positive", n);
+	}
+	else if (n < 0)
+	{
+	printf("%d is negative", n);
+	}
+	else
+	{
+	printf("%d is zero", n);
+	}
+
+	return (0);
+}
+
