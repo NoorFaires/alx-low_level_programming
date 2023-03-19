cb1eefe45c085f928a0bb636df8e2004d6119c23
Noor <nourelhoudalfaires@gmail.com>
2023-03-19 11:01:58 -0700

    ifelsecommit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry pont
+ * Description: A program that prints alphabet in lowercase
+ * return: Returns 0 (success)
+*/
+
+int main(void)
+{
+	char c = 'a';
+while (c <= 'z')
+{ putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
