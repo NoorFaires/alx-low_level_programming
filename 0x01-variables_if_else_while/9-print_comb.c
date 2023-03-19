467c2e184708873c06e926b978cf1e953a806275
Noor <nourelhoudalfaires@gmail.com>
2023-03-19 13:17:27 -0700

    ifelsecommit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints all possible combinations of single-digit numbers.
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int c = '1';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+
+
+	}
+	putchar('\n');
+	return (0);
+}
