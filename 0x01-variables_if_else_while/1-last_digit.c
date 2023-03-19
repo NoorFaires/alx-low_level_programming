b63762925c3a8014ffbf49b3ab966b6f06709e8f
Noor <nourelhoudalfaires@gmail.com>
2023-03-19 10:29:52 -0700

    ifelsecommit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,32 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A c program that examine the last digit of a number
+ *
+ * Return: returns 0 (success)
+*/
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
+	char str[] = "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 5)
+	if (n % 10 > 5)
 	{
-		printf("")
+		printf("%s %d is %d and is greater than 5\n", str, n, n % 10);
+	}
+	else if (n % 10 == 0)
+	{
+		printf("%s %d is %d and is 0\n", str, n, n % 10);
+	}
+	else
+	{
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, n % 10);
 	}
 	return (0);
 }
