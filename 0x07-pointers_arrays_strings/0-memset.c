24848eba22498f0f5ebee029ea970974e0cff339
Noor <nourelhoudalfaires@gmail.com>
2023-04-03 17:45:51 -0700

    1-memcpy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 
 /**
  * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
  *
  * Return: pointer to the memory area s
  */
