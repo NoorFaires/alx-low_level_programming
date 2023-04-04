b6ee98342ff24f3f2d453d8baa477a4c78a36ec0
Noor <nourelhoudalfaires@gmail.com>
2023-04-03 18:50:47 -0700

    0x07

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,15 +10,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
+	int i, j, f, flag;
+
+	f = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != s[i]; j++)
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (accept[j] == '\0')
-				return (i)
+			if (s[i] == accept[j])
+			{
+				f++;
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+		{
+			return (f);
 		}
 	}
-	return (i);
+
+	return (0);
 }
