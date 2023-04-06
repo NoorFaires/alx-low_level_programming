4a240ef20d3b61e5c01277b19819be880be3153b
Noor <nourelhoudalfaires@gmail.com>
2023-04-06 15:16:31 -0700

    0x08

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s : string input
+ *
+ * Return : Nothing
+*/
+
+void _puts_recursion(char *s)
+{
+	int len, i;
+
+	len = 0;
+	while (s != '\0')
+		len++;
+	for (i = 0; i < len; i++)
+		_putchar(s[i]);
+	_putchar('\n');
+}
+
