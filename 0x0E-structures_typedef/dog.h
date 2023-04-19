40837356a46083ed472f91c0cae694946b79b18c
Noor <nourelhoudalfaires@gmail.com>
2023-04-18 17:10:11 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: struct for dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _dog_h_ */
