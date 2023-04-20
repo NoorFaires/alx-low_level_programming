c5fa992db415230f88ac36565d863b457b1600ed
Noor <nourelhoudalfaires@gmail.com>
2023-04-19 17:13:36 -0700

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+
+/**
+* main - Entry point
+*
+* @argc: number of arguments
+*
+* @argv: double pointer to  arguments
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int (*func_op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	func_op = get_op_func(argv[2]);
+
+	if (func_op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", func_op(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+}
