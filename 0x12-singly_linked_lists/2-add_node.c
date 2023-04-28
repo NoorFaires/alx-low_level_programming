9c8a79e078bdcd0cd67db93ad652f0891bdf1259
Noor <nourelhoudalfaires@gmail.com>
2023-04-28 09:19:13 -0700

    Task3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,8 @@ int _strlen(const char *s)
  * add_node - add a new node at beginning of a list_t list.
  * @head: head of a list_t list.
  * @str: value to insert into element.
- * Return: the number of nodes.
+ * Return: the .s
+ * inter to the new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
