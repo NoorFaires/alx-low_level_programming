5fd7ef6e45e7b281dfe1dff107baf9bf8c371d1d
Noor <nourelhoudalfaires@gmail.com>
2023-05-05 15:57:33 -0700

    task0

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,47 +1,15 @@
 #include "main.h"
-/**
- * power_func - calculates number with base and power
- * @base: the base
- * pow: the power
- * Return: value of base and power
- */
-unsigned long int power_func(unsigned int base, unsigned int pow)
-{
-	unsigned long int num;
-	unsigned int i;
 
-	num = 1;
-	for (i = 1; i <= pow; i++)
-		num *= base;
-	return (num);
-}
 /**
- * print_binary - prints the binary representation of a number
- * @n: the number to print
- * Return: void
- */
+*print_binary - prints the binary representation of a number
+*@n: number to be printed
+*Return: void
+*/
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int power_res, result;
-	char flag;
+if (n > 1)
+	print_binary(n >> 1);
 
-	flag = 0;
-	power_res = power_func(2, sizeof(unsigned long int) * 8 - 1);
-
-	while (power_res != 0)
-	{
-		result = n & power_res;
-		if (result == power_res)
-		{
-			flag = 1;
-			_putchar('1');
-
-		}
-		else if (flag == 1 || power_res == 1)
-		{
-			_putchar('0');
-		}
-		power_res >>= 1;
-	}
+_putchar((n & 1) + '0');
 }
-
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,7 +5,6 @@
 #include <limits.h>
 
 int _putchar(char c);
-unsigned long int power_func(unsigned int base, unsigned int pow);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
