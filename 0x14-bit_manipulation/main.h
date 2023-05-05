7e58eb7d15541c4c379acf16b3e0d6be8d11e38b
Noor <nourelhoudalfaires@gmail.com>
2023-05-05 15:50:51 -0700

    task1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,8 +5,8 @@
 #include <limits.h>
 
 int _putchar(char c);
-unsigned int binary_to_uint(const char *b);
 unsigned long int power_func(unsigned int base, unsigned int pow);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
