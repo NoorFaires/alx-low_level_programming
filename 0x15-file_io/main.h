ae00934e653c6f0177bb44eb8fa800ce1d160ee4
Noor <nourelhoudalfaires@gmail.com>
2023-05-09 12:52:59 -0700

    task0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
